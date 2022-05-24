ac20f445291f30a22fd8260424863f4fa08c981b
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-05-24 15:29:28 -0400

    to hell with javascript

--- digital_business_card/homePageJS.js
+++ digital_business_card/homePageJS.js
@@ -1,7 +1,24 @@
+
+let screenHigh = window.innerHeight
+let screenWide = window.innerWidth
+
+let xPosition = Math.floor(Math.random() * screenWide);
+let yPosition = Math.floor(Math.random() * screenHigh);
+
+let backgroundWord = document.getElementById('background-words');
+
+
 let floatWords = document.getElementsByClassName('word-float');
 let floatArray = []
+
 for (i = 0; i < floatWords.length; i++) {
-    let floatText = floatWords[i].innerText;
+    let floatText = floatWords[i];
     floatArray.push(floatText)
 }
-console.log(floatArray)
+
+for (i = 0; i < floatArray.length; i++) {
+
+}
+
+
+console.log(xPosition, yPosition)
